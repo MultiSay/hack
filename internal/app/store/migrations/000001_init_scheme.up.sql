8edf45346d0493f1db910e639130ffa6dfd705cf
Maksim Kotov <ma.kotov@x5.ru>
2022-06-10 20:49:20 +0300

    db restart no

--- internal/app/store/migrations/000001_init_scheme.up.sql
+++ internal/app/store/migrations/000001_init_scheme.up.sql
@@ -3,6 +3,5 @@
 CREATE TABLE IF NOT EXISTS "hack"."user" (
     "user_id" serial NOT NULL,
     "created_at" timestamp(0) with time zone NOT NULL DEFAULT now(),
-    "updated_at" timestamp(0) with time zone,
-    ON UPDATE NO ACTION ON DELETE NO ACTION
+    "updated_at" timestamp(0) with time zone
 );
