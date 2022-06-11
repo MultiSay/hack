551d3d9949737817c79d10ade72241614d2f227e
Maksim Kotov <ma.kotov@x5.ru>
2022-06-11 21:21:11 +0300

    upd swagger

--- docs/docs.go
+++ docs/docs.go
@@ -238,7 +238,7 @@ type swaggerInfo struct {
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
 	Version:     "1.0",
-	Host:        "localhost:8080",
+	Host:        "51.250.44.134",
 	BasePath:    "/",
 	Schemes:     []string{},
 	Title:       "hack API",
