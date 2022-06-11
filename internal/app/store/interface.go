3a297bb2845c8af0e22d16a6ce7468e319cd5bfa
Maksim Kotov <ma.kotov@x5.ru>
2022-06-11 13:47:03 +0300

    refactoring

--- internal/app/store/interface.go
+++ internal/app/store/interface.go
@@ -5,6 +5,13 @@ import (
 	"hack/internal/app/model"
 )
 
+// Инициализируем репозитории
+//go:generate mockery --name=Store --structname=Store
+type Store interface {
+	File() FileRepository
+	Region() RegionRepository
+}
+
 //go:generate mockery --name=FileRepository --structname=FileRepository
 type FileRepository interface {
 	Create(context.Context, *model.File) error
@@ -12,6 +19,7 @@ type FileRepository interface {
 	GetByID(context.Context, int) (*model.File, error)
 }
 
+//go:generate mockery --name=RegionRepository --structname=RegionRepository
 type RegionRepository interface {
 	PredictList(context.Context) ([]model.RegionPredict, error)
 }
--- internal/app/store/store.go
+++
@@ -1,8 +0,0 @@
-package store
-
-// Инициализируем репозитории
-//go:generate mockery --name=Store --structname=Store
-type Store interface {
-	File() FileRepository
-	Region() RegionRepository
-}
