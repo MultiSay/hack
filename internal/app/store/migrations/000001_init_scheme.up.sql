22421008cd568e0c9d939a8b6600b3cf5c6f92ef
Maksim Kotov <ma.kotov@x5.ru>
2022-06-12 10:54:12 +0300

    migrations upd

--- internal/app/store/migrations/000001_init_scheme.up.sql
+++ internal/app/store/migrations/000001_init_scheme.up.sql
@@ -1,7 +1,21 @@
--- Доступные офферы
+CREATE TABLE "public"."files" (
+    "id" int4 NOT NULL DEFAULT nextval('files_id_seq' :: regclass),
+    "name" varchar NOT NULL,
+    "create_at" timestamptz NOT NULL DEFAULT now(),
+    "send_at" timestamptz,
+    "received_at" timestamptz,
+    "size" int8 NOT NULL,
+    "status" varchar NOT NULL DEFAULT '' :: character varying,
+    PRIMARY KEY ("id")
+);
 
-CREATE TABLE IF NOT EXISTS "hack"."user" (
-    "user_id" serial NOT NULL,
-    "created_at" timestamp(0) with time zone NOT NULL DEFAULT now(),
-    "updated_at" timestamp(0) with time zone
+CREATE TABLE "public"."region_predict" (
+    "id" int4 NOT NULL DEFAULT nextval('region_predict_id_seq' :: regclass),
+    "position" int4 NOT NULL,
+    "city" varchar NOT NULL,
+    "current_client_index" int4 NOT NULL DEFAULT 0,
+    "predict_client_index" int4 NOT NULL DEFAULT 0,
+    "predict_arpu" int4 NOT NULL DEFAULT 0,
+    "predict_score" int4 NOT NULL DEFAULT 0,
+    PRIMARY KEY ("id")
 );
